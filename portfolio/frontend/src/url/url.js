e3ad61bd79e1c5b20081ea64007f5803e345f38d
zamanmudassir200 <zamanmudassir200@gmail.com>
2025-04-17 12:19:40 +0500

    url changed

--- portfolio/frontend/src/url/url.js
+++ portfolio/frontend/src/url/url.js
@@ -1,3 +1,3 @@
 // const url = `http://localhost:3030`;
-const url = `https://reactjs-courses-project-u6ks.vercel.app`;
+const url = `https://reactjs-courses-project-ygmw.vercel.app`;
 export default url;
